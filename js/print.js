9001b808d1537aa7a9b3dd685349920eb027f20c
izzatulislam <yayasan@izzatulislam.org>
2025-01-08 13:54:32 +0700

    Update print.js

--- js/print.js
+++ js/print.js
@@ -21,7 +21,7 @@ function printSPPD() {
     const nominalMenginap = menginap * 100000;
 
     // Buat URL untuk halaman print dengan query string
-    const url = `print-layout.php?jarak=${jarak}&nominalJarak=${nominalJarak}&durasi=${durasi}&nominalDurasi=${nominalDurasi}&snack=${snack}&nominalSnack=${nominalSnack}&makan=${makan}&nominalMakan=${nominalMakan}&lembur=${lembur}&nominalLembur=${nominalLembur}&menginap=${menginap}&nominalMenginap=${nominalMenginap}&total=${total}&totalTerbilang=${totalTerbilang}&catatan=${encodeURIComponent(catatan)}&keteranganTambahan=${encodeURIComponent(keteranganTambahan)}`;
+    const url = `print-layout.html?jarak=${jarak}&nominalJarak=${nominalJarak}&durasi=${durasi}&nominalDurasi=${nominalDurasi}&snack=${snack}&nominalSnack=${nominalSnack}&makan=${makan}&nominalMakan=${nominalMakan}&lembur=${lembur}&nominalLembur=${nominalLembur}&menginap=${menginap}&nominalMenginap=${nominalMenginap}&total=${total}&totalTerbilang=${totalTerbilang}&catatan=${encodeURIComponent(catatan)}&keteranganTambahan=${encodeURIComponent(keteranganTambahan)}`;
     
     // Buka halaman print di tab baru
     window.open(url, '_blank');
